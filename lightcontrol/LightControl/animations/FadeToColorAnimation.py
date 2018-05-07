b850187ff809d99156c21ec6220ba9547ad40f85
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2018-05-07 10:43:29 -0400

    Add sTime to FadeToColor

--- lightcontrol/LightControl/animations/FadeToColorAnimation.py
+++ lightcontrol/LightControl/animations/FadeToColorAnimation.py
@@ -9,9 +9,10 @@ TS_TIME = 50
 from .base.BaseAnimation import BaseAnimation
 
 class FadeToColorAnimation(BaseAnimation):
-	def __init__(self, lights=[], colors=[], numCycles=1, speed=1, brightness=255, tsTime=TS_TIME):
+	def __init__(self, lights=[], colors=[], numCycles=1, speed=1, brightness=255, tsTime=TS_TIME, sTime=None):
 		self._brightness = brightness # Brightness
 		self._tsTime = tsTime
+		self._sTime = sTime
 		super().__init__(lights, colors, numCycles, speed)
 
 	def _generateAnimationSequence(self):
@@ -19,6 +20,8 @@ class FadeToColorAnimation(BaseAnimation):
 		for light in self._lights:
 			if light == self._lights[-1]:
 				sTime = self._tsTime/6.5
+				if self._sTime != None:
+					sTime = self._sTime
 				step = self.createStep(light, self._colors[0], bri=self._brightness, tsTime=self._tsTime, sTime=sTime)
 			else:
 				step = self.createStep(light, self._colors[0], bri=self._brightness, tsTime=self._tsTime, sTime=0)
